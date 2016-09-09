5dce362b3271cc489b2bb043dd8cbf5b4401ba4b
lordshadowist <shadowist@live.com>
2016-09-09 15:59:03 -0700

    Bill Cheng debug Finish

--- buggy-script/js/app.js
+++ buggy-script/js/app.js
@@ -1,8 +1,8 @@
-widow.onload = function(){
+window.onload = function(){
   var main = document.getElementById('main');
   var roll = document.getElementById('roll');
   var add  = document.getElementById('add');
-  add.addEventListener("cluck", function(){
+  add.addEventListener("click", function(){
     addDie();
   });
   roll.addEventListener("click", function(){
@@ -17,13 +17,42 @@ widow.onload = function(){
   };
 
   function rollDie() {
-    var dice = document.getElementByClassName('die');
-    for (var i = 0; i < dice.length; i++) {
-      dice[i].innerHTML = Random();
+    var dice = document.getElementsByClassName('die');
+    for (var i = 0; i < dice.length; i++) 
+    {
+      dice[i].innerHTML = random();
     };
+    checkForWin();
   };
 
-  function random(){
-    return Math.floor(Math.random() * (7));
+  function checkForWin()
+  {
+    var win = false;
+    var dice = document.getElementsByClassName("die");
+    if(dice.length > 1)
+    {
+      win = true;
+      var firstDie = dice[0];
+      for(var i = 1; i < dice.length; i++)
+      {
+        if (dice[i].innerHTML != firstDie.innerHTML)
+        {
+            win = false;
+        }
+      }
+    }
+    if(win)
+    {
+      var main = document.getElementById("main");
+      main.style.background = "url(youwin.jpg)"
+      main.style.backgroundRepeat = "no-repeat";
+      main.style.backgroundPosition = "center top";
+      main.style.backgroundSize = "1000px 1000px";
+    }
+  }
+
+  function random()
+  {
+    return Math.floor(Math.random() * (6) + 1);
   }
 };
